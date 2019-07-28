7a0ced50f949c5b767921fb013b51effd34e060e
Oleg Yaroshevich <oleg.jar@gmail.com>
2019-07-28 11:17:38 +0300

    Small palindrome update

--- src/com/javaguru/lessons/lesson7/PalindromeDemo.java
+++ src/com/javaguru/lessons/lesson7/PalindromeDemo.java
@@ -3,6 +3,6 @@ package com.javaguru.lessons.lesson7;
 public class PalindromeDemo {
     public static void main(String[] args) {
         Palindrome palindrome = new Palindrome();
-        palindrome.isPalindrome("RACEC;AR");
+        palindrome.isPalindrome("R[]a CEC;AR");
     }
 }
