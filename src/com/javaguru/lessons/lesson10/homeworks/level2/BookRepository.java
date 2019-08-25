42dc062e88282d10e7a63b628f12e7c8dff32608
Oleg Yaroshevich <oleg.jar@gmail.com>
2019-08-25 08:42:48 +0300

    small update in BookRepository

--- src/com/javaguru/lessons/lesson10/homeworks/level2/BookRepository.java
+++ src/com/javaguru/lessons/lesson10/homeworks/level2/BookRepository.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class BookRepository implements Repository<Book> {
 
-    Map<String, Book> bookRepository = new HashMap<>();
+    private Map<String, Book> bookRepository = new HashMap<>();
 
     @Override
     public List<Book> findAll() {
